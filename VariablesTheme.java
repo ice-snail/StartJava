4c27cca5e75fb20b63a616b3c49bbeadbb32b42f
ice-snail <krasukov95@gmail.com>
2024-10-24 23:24:21 +0400

    fixed bugs in VariablesTheme.java

--- VariablesTheme.java
+++ VariablesTheme.java
@@ -40,119 +40,118 @@ class VariablesTheme {
 
         System.out.println("4. Вывод min и max значений целых числовых типов");
         byte maxByte = 127;
-        short maxShort = 32767;
-        int maxInt = 2147483647;
-        long maxLong = 9223372036854775807L;
         System.out.println(maxByte);
         System.out.println(++maxByte);
         System.out.println(--maxByte + "\n");
 
+        short maxShort = 32767;
         System.out.println(maxShort);
         System.out.println(++maxShort);
         System.out.println(--maxShort + "\n");
 
+        char maxChar = 65535;
+        System.out.println(maxChar);
+        System.out.println(++maxChar);
+        System.out.println(--maxChar + "\n");
+
+        int maxInt = 2147483647;
         System.out.println(maxInt);
         System.out.println(++maxInt);
         System.out.println(--maxInt + "\n");
 
+        long maxLong = 9223372036854775807L;
         System.out.println(maxLong);
         System.out.println(++maxLong);
         System.out.println(--maxLong + "\n");
 
         System.out.println("5. Перестановка значений переменных");
-        int first = 2;
-        int second = 5;
-        int third = 0;
-        System.out.println("Первая переменная = " + first);
-        System.out.println("Вторая переменная = " + second);
+        int a = 2;
+        int b = 5;
+        
+        System.out.println("Первая переменная = " + a);
+        System.out.println("Вторая переменная = " + b);
 
         System.out.println("Перестановка с помощью третьей переменной");
-        third = first;
-        first = second;
-        second = third;
-        System.out.println("В результате перестановки первая переменная = " + first + 
-                ", вторая переменная = " + second + "\n");
+        int swap = a;
+        a = b;
+        b = swap;
+        System.out.println("В результате перестановки первая переменная = " + a + 
+                ", вторая переменная = " + b + "\n");
 
         System.out.println("Перестановка с помощью арифметических операций");
-        int sumVariables = first + second;
-        first = sumVariables - first;
-        second = sumVariables - second;
-        System.out.println("В результате перестановки первая переменная = " + first + 
-                ", вторая переменная = " + second + "\n");
+        a *= b;
+        b = a / b;
+        a /= b;
+        System.out.println("В результате перестановки первая переменная = " + a + 
+                ", вторая переменная = " + b + "\n");
 
         System.out.println("Перестановка с помощью побитовой операции ^");
-        int xor = first ^ second;
-        first = xor - first;
-        second = xor - second;
-        System.out.println("В результате перестановки первая переменная = " + first + 
-                ", вторая переменная = " + second + "\n");
+        a ^= b;
+        b ^= a;
+        a ^= b;
+        System.out.println("В результате перестановки первая переменная = " + a + 
+                ", вторая переменная = " + b + "\n");
 
         System.out.println("6. Вывод символов и их кодов");
-        char chNum36 = '$';
-        char chNum42 = '*';
-        char chNum64 = '@';
-        char chNum124 = '|';
-        char chNum126 = '~';
-        int codeChNum36 = chNum36;
-        int codeChNum42 = chNum42;
-        int codeChNum64 = chNum64;
-        int codeChNum124 = chNum124;
-        int codeChNum126 = chNum126;
-        System.out.println(codeChNum36 + " - " + chNum36);
-        System.out.println(codeChNum42 + " - " + chNum42);
-        System.out.println(codeChNum64 + " - " + chNum64);
-        System.out.println(codeChNum124 + " - " + chNum124);
-        System.out.println(codeChNum126 + " - " + chNum126 + "\n");
+        char dollar = '$';
+        char asterisk = '*';
+        char atSign = '@';
+        char verticalBar = '|';
+        char tilde = '~';
+        System.out.println((int) dollar + " - " + dollar);
+        System.out.println((int) asterisk + " - " + asterisk);
+        System.out.println((int) atSign + " - " + atSign);
+        System.out.println((int) verticalBar + " - " + verticalBar);
+        System.out.println((int) tilde + " - " + tilde + "\n");
 
         System.out.println("7. Вывод в консоль ASCII-арт Дюка");
         char slash = '/';
-        char reverseSlash = '\\';
-        char underline = '_';
-        char parenthesis = '(';
-        char reverseParenthesis = ')';
-        System.out.println("    " + slash + reverseSlash);
-        System.out.println("   " + slash + "  " + reverseSlash);
-        System.out.println("  " + slash + underline + parenthesis + " " + reverseParenthesis +
-                reverseSlash);
-        System.out.println(" " + slash + "      " + reverseSlash);
-        System.out.println(slash + "" + underline + "" + underline + "" + underline + 
-                "" + underline + "" + slash + "" + reverseSlash + "" + underline + 
-                "" + underline + "" + reverseSlash + "\n");
+        char backslash = '\\';
+        char underscore = '_';
+        char leftParenthesis = '(';
+        char rightParenthesis = ')';
+        System.out.println("    " + slash + backslash);
+        System.out.println("   " + slash + "  " + backslash);
+        System.out.println("  " + slash + underscore + leftParenthesis + " " + rightParenthesis +
+                backslash);
+        System.out.println(" " + slash + "      " + backslash);
+        System.out.println("" + slash + underscore + underscore + underscore + underscore + slash + 
+                backslash + underscore + underscore + backslash + "\n");
 
         System.out.println("8. Манипуляции с сотнями, десятками и единицами числа");
         int someNum = 123;
         int hundreds = someNum / 100;
-        int decades = someNum % 100 / 10;
-        int units = someNum % 100 % 10;
-        int sumCapacity = hundreds + decades + units;
-        int productCapacity = hundreds * decades * units;
+        int tens = someNum % 100 / 10;
+        int ones = someNum % 10;
+        int sum = hundreds + tens + ones;
+        int product = hundreds * tens * ones;
         System.out.println("Число " + someNum + " содержит:");
         System.out.println("сотен - " + hundreds);
-        System.out.println("десятков - " + decades);
-        System.out.println("единиц - " + units);
-        System.out.println("Сумма разрядов = " + sumCapacity);
-        System.out.println("Произведение разрядов = " + productCapacity + "\n");
+        System.out.println("десятков - " + tens);
+        System.out.println("единиц - " + ones);
+        System.out.println("Сумма разрядов = " + sum);
+        System.out.println("Произведение разрядов = " + product + "\n");
 
         System.out.println("9. Перевод секунд в часы, минуты и секунды");
-        int someCountSeconds = 86399;
+        int totalSeconds = 86399;
 
         // В одном часе 60 минут, а в одной минуте 60 секунд, считаем количество секунд в часе:
         int secondsInHours = 60 * 60;
-        int hours = someCountSeconds / secondsInHours;
-        int minutes = someCountSeconds % secondsInHours / 60;
-        int seconds = someCountSeconds % secondsInHours % 60;
-        System.out.println(hours + ":" + minutes + ":" + seconds + "\n");
+        int hh = totalSeconds / secondsInHours;
+        int mm = totalSeconds % secondsInHours / 60;
+        int ss = totalSeconds % secondsInHours % 60;
+        System.out.println(hh + ":" + mm + ":" + ss + "\n");
 
         System.out.println("10. Расчет стоимости товара со скидкой");
         var penPriceBd = new BigDecimal("105.50");
         var bookPriceBd = new BigDecimal("235.83");
         var discountBd = new BigDecimal("0.11");
-        var sumPriceBd = penPriceBd.add(bookPriceBd);
-        var sumDiscountBd = sumPriceBd.multiply(discountBd).setScale(2, RoundingMode.HALF_UP);
-        var discountPriceBd = sumPriceBd.subtract(sumDiscountBd);
+        var basePriceBd = penPriceBd.add(bookPriceBd);
+        var sumDiscountBd = basePriceBd.multiply(discountBd).setScale(2, RoundingMode.HALF_UP);
+        var discountPriceBd = basePriceBd.subtract(sumDiscountBd);
         System.out.println("Стоимость ручки = " + penPriceBd);
         System.out.println("Стоимость книги = " + bookPriceBd);
-        System.out.println("Стоимость товара без скидки = " + sumPriceBd);
+        System.out.println("Стоимость товара без скидки = " + basePriceBd);
         System.out.println("Сумма скидки = " + sumDiscountBd);
         System.out.println("Стоимость товаров со скидкой = " + discountPriceBd);
     }
